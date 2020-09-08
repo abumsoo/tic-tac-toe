f194ef33240dea2ab7b02d7e481f7e6e92c2baa0
Bum Kim <bumcrystlbum@gmail.com>
2020-09-08 08:41:39 -0400

    need to allow players to input names

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,133 @@
-// gameboard module
-const gameBoard = (function() {
-  let _gameboard = [
-    "X","O","X",
-    "O", "X", "O",
-    "O", "X", "O"
-  ];
+// Gameboard module
+const gameboard = (function () {
+  let _gameboard = ['','','','','','','','',''];
 
-  function getGameboard() {
-    return _gameboard;
+  let lastTurn = 'O';
+
+  const getGameboard = () => _gameboard;
+
+  const isFull = () => {
+    return _gameboard.reduce((bool, value) => {
+      return (bool && !isEmpty(value));
+    }, true);
   }
 
-  return {
-    getGameboard
+  const checkRow = (index) => {
+    let rowIndex = Math.floor(index/3)*3;
+    if (_gameboard[rowIndex] === _gameboard[rowIndex+1] &&
+        _gameboard[rowIndex] === _gameboard[rowIndex+2]) {
+      return true;
+    }
+    return false;
   };
-})();
 
-// game flow display module
-const displayController = (function () {
+  const checkCol = (index) => {
+    let colIndex = index % 3;
+    if (_gameboard[colIndex] === _gameboard[colIndex+3] &&
+        _gameboard[colIndex] === _gameboard[colIndex+6]) {
+      return true;
+    }
+    return false;
+  };
 
-  let drawBoard = (gameboard) => {
-    let cells = document.querySelectorAll(".cell");
-    for (let i = 0; i < 9; i++) {
-      cells[i].innerHTML = gameboard[i];
+  const checkDiag = (index) => {
+    // check top left to bottom right
+    if (index == 0 || index == 8) {
+      return (_gameboard[0] === _gameboard[4]) &&
+        (_gameboard[0] === _gameboard[8])
+    } else if (index == 6 || index == 2) {
+      return (_gameboard[6] === _gameboard[4]) &&
+        (_gameboard[2] === _gameboard[4])
+    } else if (index == 4) {
+      return ((_gameboard[0] === _gameboard[4]) &&
+              (_gameboard[0] === _gameboard[8])) ||
+        ((_gameboard[6] === _gameboard[4]) &&
+         (_gameboard[2] === _gameboard[4]))
+    }
+    return false;
+  };
+
+  const isGameover = (index) => {
+    if (isFull()) {
+      return true;
+    }
+    if (index % 2 !== 0) {
+      return checkRow(index) || checkCol(index);
+    } else {
+      return checkRow(index) || checkCol(index) || checkDiag(index);
     }
   };
 
-  return {
-    drawBoard
+  const isEmpty = (value) => {
+    return !(value || value.length);
+  }
+
+  const clear = () => {
+    _gameboard = _gameboard.map(cell => '');
+    flowControl.render();
   };
+
+  const setCell = (e) => {
+    let index = e.target.getAttribute("data-index");
+    if (isEmpty(_gameboard[index])) {
+      if (lastTurn === 'X') {
+        _gameboard[index] = 'O';
+      } else {
+        _gameboard[index] = 'X';
+      }
+      lastTurn = _gameboard[index];
+      flowControl.render();
+      if (isGameover(index)) {
+        const text = document.createElement('p');
+        text.innerHTML = "Game over!";
+        document.body.appendChild(text);
+      }
+    }
+  };
+
+  const addListeners = () => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => cell.addEventListener('click', setCell));
+  };
+  return {
+    getGameboard,
+    addListeners,
+    clear
+  }
 })();
 
-// player factory func
-const Player = () => {
-
+// player factory
+const Player = (name) => {
+  let _name = name || "Joe";
   return {
 
-  };
+  }
 };
 
-let gameboard = gameBoard.getGameboard();
-displayController.drawBoard(gameboard);
+const flowControl = (function () {
+  const init = () => {
+    addMainListeners();
+    render();
+  };
+
+  const addMainListeners = () => {
+    const startBtn = document.querySelector(".start");
+    startBtn.addEventListener('click', gameboard.addListeners);
+    const restartBtn = document.querySelector(".restart");
+    restartBtn.addEventListener('click', gameboard.clear);
+  };
+
+  const render = () => {
+    const cells = document.querySelectorAll(".cell");
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].innerHTML = gameboard.getGameboard()[i];
+    }
+  }
+
+  return {
+    init,
+    render
+  }
+})();
+
+flowControl.init();
